b541fed180bb5ed225dcdff7f0cd82895965881d
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-08 19:42:22 +0700

    Update application.class.php

--- application.class.php
+++ application.class.php
@@ -49,9 +49,9 @@ class Application {
                 $trim = substr($metaString, $position0);
                 $position1 = stripos($trim, ':') + 2;
                 $trim = substr($trim, $position1);
-                $position2 = -stripos($trim, ':');
+                $position2 = stripos($trim, ':');
                 $trim = substr($trim, $position1, $position2);
-                return $trim;
+                return $position2;
         }
 }
 ?>
