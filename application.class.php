5d5b987be7aa8152209a79c82850931873f3e6c4
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-08 19:44:12 +0700

    Update application.class.php

--- application.class.php
+++ application.class.php
@@ -50,8 +50,8 @@ class Application {
                 $position1 = stripos($trim, ':') + 2;
                 $trim = substr($trim, $position1);
                 $position2 = stripos($trim, ':');
-                $trim = substr($trim, $position1, $position2);
-                return $position2;
+                $trim1 = substr($trim, $position1, $position2);
+                return $trim . ' ' . $trim1;
         }
 }
 ?>
