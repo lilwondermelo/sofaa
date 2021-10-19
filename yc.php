8fef3d238a4224dd1342663399b28e4884b426f3
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-19 19:02:57 +0700

    Update yc.php

--- yc.php
+++ yc.php
@@ -39,9 +39,42 @@
 	$result = json_decode($out,TRUE);
 
 
-
+	$i = 0;
 	foreach ($result['data'] as $item) {
-		$data[] = $item['id'];
+		$type = 'GET';
+		$link = 'https://api.yclients.com/api/v1/client/505278/' . $item['id'];
+		$curl=curl_init();
+	curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+	switch (mb_strtoupper($type)) { 
+		case 'GET':
+			$link .= "?".http_build_query($args);
+			curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
+			break; 
+		case 'POST':
+			curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+			curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($args));
+			break; 
+		case 'PUT':
+			curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
+			curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($args));
+			break; 
+		default: 
+			curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $type); 
+	}
+	curl_setopt($curl,CURLOPT_URL,$link);
+	curl_setopt($curl,CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($curl,CURLOPT_HEADER,false);
+	curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0);
+	curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,0);
+	$out=curl_exec($curl);
+	$code=curl_getinfo($curl,CURLINFO_HTTP_CODE);  
+	curl_close($curl);
+	$res = json_decode($out,TRUE);
+	$data[$i] = $res['name'];
+	$i++;
+	if ($i%5 == 0) {
+		sleep(1);
+	}
 	}
 
 	echo json_encode($data);
