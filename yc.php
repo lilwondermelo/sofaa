6ac0b84028dd35164b39aef04052030811a7ba30
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-19 19:16:41 +0700

    Update yc.php

--- yc.php
+++ yc.php
@@ -77,10 +77,40 @@
 	}
 	}
 
-	echo json_encode($data, JSON_UNESCAPED_UNICODE);
+	//echo json_encode($data, JSON_UNESCAPED_UNICODE);
 
 
+	$link='https://'.$host.'.amocrm.ru/api/v4/contacts';
 
+	$curl = curl_init(); //Сохраняем дескриптор сеанса cURL
+			curl_setopt($curl,CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($curl,CURLOPT_USERAGENT,'amoCRM-oAuth-client/1.0');
+			curl_setopt($curl,CURLOPT_URL, $link);
+			curl_setopt($curl,CURLOPT_HTTPHEADER,['Content-Type:application/json']);
+			curl_setopt($curl,CURLOPT_HTTPHEADER,['Authorization:Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImQ4ZWJjYzdlMzFhY2ZjMWIzODA3ZDgzNjUyZjdhOTA3Yjk1MTI2MjQxMzgyZDQwZDZiMTZmODY0MjgwZGM0NDQwNmJjYzg3OWM5YTI2NzU4In0.eyJhdWQiOiJiMjVlMWIwMC1iZDI3LTRkMTAtOTM2My1hNWZkYjMxMDI3YjAiLCJqdGkiOiJkOGViY2M3ZTMxYWNmYzFiMzgwN2Q4MzY1MmY3YTkwN2I5NTEyNjI0MTM4MmQ0MGQ2YjE2Zjg2NDI4MGRjNDQ0MDZiY2M4NzljOWEyNjc1OCIsImlhdCI6MTYzNDY0MTA5NSwibmJmIjoxNjM0NjQxMDk1LCJleHAiOjE2MzQ3Mjc0OTUsInN1YiI6IjM0OTMwNTciLCJhY2NvdW50X2lkIjoyOTc2NTIzMCwic2NvcGVzIjpbInB1c2hfbm90aWZpY2F0aW9ucyIsImNybSIsIm5vdGlmaWNhdGlvbnMiXX0.UltDqHpRlQwtWjZN0QnV4rvJcwJ0HkmTA5oSzxebnZrbLsPHlVHqBqkkEjjpZWfuLyIBeH5HznTb5liYJV9IrkAd96GsWnFODCJi00mRY9faoNx3-rIQSKoVQnioILk10X8JZhkRHlOZEv181JZ8Zvh-uG0C2nLA_T7d4dbpMtjp-mJUqhbwOoKzUu0rSHHIefufjOqwP-ZhLMkm8lWfSLgdwcp_CMjbn2wL_VScRzD1UhrZBjgzCM7C1NqqkzSzVh7fM6Ebb0849pCPU-2vj-Ibp8f-E7GuBeFixhKidz3Hzi981C6vhjbglMEJSfo8-AbP82JaYxkT8Xt6xeePbA']);
+			curl_setopt($curl,CURLOPT_HEADER, false);
+			curl_setopt($curl,CURLOPT_CUSTOMREQUEST, 'POST');
+			curl_setopt($curl,CURLOPT_POSTFIELDS, json_encode($data));
+			curl_setopt($curl,CURLOPT_SSL_VERIFYPEER, 1);
+			curl_setopt($curl,CURLOPT_SSL_VERIFYHOST, 2);
+			$out = curl_exec($curl); //Инициируем запрос к API и сохраняем ответ в переменную
+			$code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+			curl_close($curl);
+			/** Теперь мы можем обработать ответ, полученный от сервера. Это пример. Вы можете обработать данные своим способом. */
+			$code = (int)$code;
+			$errors = [
+				400 => 'Bad request',
+				401 => 'Unauthorized',
+				403 => 'Forbidden',
+				404 => 'Not found',
+				500 => 'Internal server error',
+				502 => 'Bad gateway',
+				503 => 'Service unavailable',
+			];
+
+			$response = json_decode($out, true);
+
+	    echo $out;
 	
 
 
