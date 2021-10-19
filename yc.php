486907eda38f37957688c76705764e41c275e908
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-19 18:52:03 +0700

    Update yc.php

--- yc.php
+++ yc.php
@@ -38,45 +38,11 @@
 	curl_close($curl);
 	$result = json_decode($out,TRUE);
 
-	//echo $result['meta']['total_count'];
-
-	$count = $result['meta']['total_count']/200;
+	echo $out;
 
 
-	for ($i = 0; $i <= $count; $i = $i++) {
-		$curl=curl_init();
-	curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
-	switch (mb_strtoupper($type)) { 
-		case 'GET':
-			$link .= "?".http_build_query($args);
-			curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
-			break; 
-		case 'POST':
-			curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
-			curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($args));
-			break; 
-		case 'PUT':
-			curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
-			curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($args));
-			break; 
-		default: 
-			curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $type); 
-	}
-	curl_setopt($curl,CURLOPT_URL,$link);
-	curl_setopt($curl,CURLOPT_HTTPHEADER, $headers);
-	curl_setopt($curl,CURLOPT_HEADER,false);
-	curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0);
-	curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,0);
-	$out=curl_exec($curl);
-	$code=curl_getinfo($curl,CURLINFO_HTTP_CODE);  
-	curl_close($curl);
-	$result = json_decode($out,TRUE);
 
-	foreach ($result['data'] as $item) {
-		$data[] = $item['id'];
-	}
-	echo json_encode($data);
-	}
+	
 
 
 	
