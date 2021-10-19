2d08daf20c5a0d590cbedaa3fb207cbcd9070663
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-19 17:49:28 +0700

    Update yc.php

--- yc.php
+++ yc.php
@@ -1,7 +1,7 @@
 <?php
 	$type = 'POST';
 	$args = array('count' => 200);
-
+	$data = array();
 	$link = 'https://api.yclients.com/api/v1/company/505278/clients/search?';
 
 	$headers = array(
@@ -41,7 +41,42 @@
 	//echo $result['meta']['total_count'];
 
 	$count = $result['meta']['total_count']/200;
-	echo $count;
+
+
+	for ($i = 0; $i <= $count; $i = $i++) {
+		$curl=curl_init();
+	curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+	switch (mb_strtoupper($type)) { 
+		case 'GET':
+			$link .= "?".http_build_query($args);
+			curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
+			break; 
+		case 'POST':
+			curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+			curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($args));
+			break; 
+		case 'PUT':
+			curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
+			curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($args));
+			break; 
+		default: 
+			curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $type); 
+	}
+	curl_setopt($curl,CURLOPT_URL,$link);
+	curl_setopt($curl,CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($curl,CURLOPT_HEADER,false);
+	curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0);
+	curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,0);
+	$out=curl_exec($curl);
+	$code=curl_getinfo($curl,CURLINFO_HTTP_CODE);  
+	curl_close($curl);
+	$result = json_decode($out,TRUE);
+
+	foreach ($result['data'] as $item) {
+		$data[] = $item['id'];
+	}
+	echo json_encode($data);
+	}
 
 
 	
