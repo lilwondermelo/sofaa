9984e18fed4055ce963d4ed55b6db299d4056554
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-20 19:00:42 +0700

    Update yc.php

--- yc.php
+++ yc.php
@@ -43,7 +43,7 @@
 
 $j = 0;
 	for ($i = 0; $i < $count; $i++) {
-		$args = array('page_size' => 5, 'page' => $i+1);
+		$args = array('page_size' => 200, 'page' => $i+1);
 		$curl=curl_init();
 	curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
 	switch (mb_strtoupper($type)) { 
