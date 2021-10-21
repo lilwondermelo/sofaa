9af4e0e99bc4cea364bc4af21e70ac8973329b06
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-21 17:05:39 +0700

    Update yc_clients.php

--- yc_clients.php
+++ yc_clients.php
@@ -3,12 +3,8 @@ $resultDb = array(); //Массив для занесения результат
 require_once 'ycClass.php'; //Класс для работы с API YCLIENTS
 $ycClass = new YCClass('ablaser'); //В конструктор класса передаем название (название - поддомен компании из AMOCRM)
 $pages = $ycClass->getCLientCount()['pages']; //Количество страниц в запросе пользователей
-for ($i = 0; $i < $pages; $i++) { //цикл перебирает страницы (API YCLIENTS не дает больше 200 значений на одну страницу)
+for ($i = 0; $i < (int)$pages; $i++) { //цикл перебирает страницы (API YCLIENTS не дает больше 200 значений на одну страницу)
 	$pageData = $ycClass->getClients($i+1); //$i+1 - номер текущей страницы
-	foreach ($pageData['data'] as $item) {
-		
-		$clientData = $ycClass->getClientData($item['id']);
-		echo json_encode($clientData) . '<br>';
-	}
+	echo json_encode($pageData) . '<br>';
 }
 ?>
