2213f8170c4ced06d8e9d2f8881211194749ae25
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-21 11:26:54 +0700

    Update amo_contacts.php

--- amo_contacts.php
+++ amo_contacts.php
@@ -28,7 +28,7 @@ $link='https://'.$hostAmo.'.amocrm.ru/api/v4/contacts';
 			$out = curl_exec($curl); //Инициируем запрос к API и сохраняем ответ в переменную
 			curl_close($curl);
 	
-			echo $out;
+			echo json_encode($data);
 	}
 
 	
