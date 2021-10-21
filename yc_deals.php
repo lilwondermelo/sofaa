b69a89665f43293493711e3a63e4e3c8e67ec798
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-21 08:51:55 +0700

    Update yc_deals.php

--- yc_deals.php
+++ yc_deals.php
@@ -1,6 +1,6 @@
 <?php
 	require_once '_dataSource.class.php';
-	$dataSource = new DataSource('select yc_id from clients_laser where yc_id = 117378729');
+	$dataSource = new DataSource('select yc_id from clients_laser');
 	$dataS = $dataSource->getData();
 	$i = 0;
 	$data = array();
