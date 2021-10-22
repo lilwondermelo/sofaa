72871634281f277f364f9e03dd94e865c5415489
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-22 13:07:38 +0700

    Update amo_deals.php

--- amo_deals.php
+++ amo_deals.php
@@ -2,6 +2,7 @@
 require_once '_dataSource.class.php';
 	$dataSource = new DataSource('select r.yc_id as recordId, r.yc_client_id as clientId, r.date_last as dateLast, r.stat as stat, r.is_deleted as isDeleted, c.name as name, c.spent as spent, c.amo_id as amoId from records_autobeauty r join clients_autobeauty c on r.yc_client_id = c.yc_id where r.stat = -1 or r.stat = 1');
 	$dataS = $dataSource->getData();
+	$sum = array();
 	$result = array();
 $hostAmo = strtolower(trim("autobeauty"));
 $link='https://'.$hostAmo.'.amocrm.ru/api/v4/leads';
@@ -45,12 +46,12 @@ $data = array();
             	curl_setopt($curl,CURLOPT_HTTPHEADER,['Authorization:Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjQ4OWVjZWYxMDBjODJlOTNjMmEzMDgyNWYwZDhiY2U0ZGRjYjc5NGI0MDVkZmYwOTgxOTBmNzllNTI0Y2Q4ZjBlOGVhZmExNzk1MzA2MTU3In0.eyJhdWQiOiJmNWMxOWRmMi0wNWU4LTQ0YmQtYWZiOC1hMGIyMDI3ODg4MWQiLCJqdGkiOiI0ODllY2VmMTAwYzgyZTkzYzJhMzA4MjVmMGQ4YmNlNGRkY2I3OTRiNDA1ZGZmMDk4MTkwZjc5ZTUyNGNkOGYwZThlYWZhMTc5NTMwNjE1NyIsImlhdCI6MTYzNDgxNTY5NywibmJmIjoxNjM0ODE1Njk3LCJleHAiOjE2MzQ5MDIwOTcsInN1YiI6IjM0OTMwNTciLCJhY2NvdW50X2lkIjoyODg1OTk3MSwic2NvcGVzIjpbInB1c2hfbm90aWZpY2F0aW9ucyIsImNybSIsIm5vdGlmaWNhdGlvbnMiXX0.IqSfMWMkPvq_gq9E-jlKJbj5sDxeFX3-zXSEABlAp255DHgSWKYDkymGc94UyiafQAqYCjIO64aPWJ-5XdrOmFaSl11m51EHePlseqDvzxMc3bQ6xKQ1hxJSPAc8SELTwBR2XirSRG0kmurWM2P7Ni-6gzc1kEoHN9GGshEzzV_IyLOvSgNmQXwP261xc2c_W2X0em_RGo7pwwKBCUs8nm35iWIWaZj1ffW1NMqVGUWWYC4i2UVhNwfeiSBOK9Bmxg7mQQE2XhZj0c21jXzAMzFVVje9lZ2OvvGCG3OOIzv7HkcUZvEDBvgIBx-Hl1UbSjmcVqMJCBs8wLbx9vsAAQ']);
             	curl_setopt($curl,CURLOPT_HEADER, false);
             	curl_setopt($curl,CURLOPT_CUSTOMREQUEST, 'POST');
-            	curl_setopt($curl,CURLOPT_POSTFIELDS, json_encode($array250));
+            	curl_setopt($curl,CURLOPT_POSTFIELDS, json_encode($data250));
             	curl_setopt($curl,CURLOPT_SSL_VERIFYPEER, 1);
             	curl_setopt($curl,CURLOPT_SSL_VERIFYHOST, 2);
             	$out = curl_exec($curl); //Инициируем запрос к API и сохраняем ответ в переменную
             	curl_close($curl);
-            	$sum = json_decode($out, true)['id'];
+            	$sum[] = json_decode($out, true)['id'];
     		
     
 
