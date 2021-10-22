e4f698cc175b867bb38abf47d386e481a32533a1
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-22 12:08:32 +0700

    Update amo_deals.php

--- amo_deals.php
+++ amo_deals.php
@@ -40,10 +40,22 @@ $data = array();
 	$data250 = array_chunk($data, 200);
 
 	foreach ($data250 as $array250) {
-		foreach ($array250 as $item) {
-			echo $item['name'] . '<br>';
-		}
-        echo count($array250) . '<br><br>';
+		foreach ($data250 as $array250) {
+                $curl = curl_init(); //Сохраняем дескриптор сеанса cURL
+            	curl_setopt($curl,CURLOPT_RETURNTRANSFER, true);
+            	curl_setopt($curl,CURLOPT_USERAGENT,'amoCRM-oAuth-client/1.0');
+            	curl_setopt($curl,CURLOPT_URL, $link);
+            	curl_setopt($curl,CURLOPT_HTTPHEADER,['Content-Type:application/json']);
+            	curl_setopt($curl,CURLOPT_HTTPHEADER,['Authorization:Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjQ4OWVjZWYxMDBjODJlOTNjMmEzMDgyNWYwZDhiY2U0ZGRjYjc5NGI0MDVkZmYwOTgxOTBmNzllNTI0Y2Q4ZjBlOGVhZmExNzk1MzA2MTU3In0.eyJhdWQiOiJmNWMxOWRmMi0wNWU4LTQ0YmQtYWZiOC1hMGIyMDI3ODg4MWQiLCJqdGkiOiI0ODllY2VmMTAwYzgyZTkzYzJhMzA4MjVmMGQ4YmNlNGRkY2I3OTRiNDA1ZGZmMDk4MTkwZjc5ZTUyNGNkOGYwZThlYWZhMTc5NTMwNjE1NyIsImlhdCI6MTYzNDgxNTY5NywibmJmIjoxNjM0ODE1Njk3LCJleHAiOjE2MzQ5MDIwOTcsInN1YiI6IjM0OTMwNTciLCJhY2NvdW50X2lkIjoyODg1OTk3MSwic2NvcGVzIjpbInB1c2hfbm90aWZpY2F0aW9ucyIsImNybSIsIm5vdGlmaWNhdGlvbnMiXX0.IqSfMWMkPvq_gq9E-jlKJbj5sDxeFX3-zXSEABlAp255DHgSWKYDkymGc94UyiafQAqYCjIO64aPWJ-5XdrOmFaSl11m51EHePlseqDvzxMc3bQ6xKQ1hxJSPAc8SELTwBR2XirSRG0kmurWM2P7Ni-6gzc1kEoHN9GGshEzzV_IyLOvSgNmQXwP261xc2c_W2X0em_RGo7pwwKBCUs8nm35iWIWaZj1ffW1NMqVGUWWYC4i2UVhNwfeiSBOK9Bmxg7mQQE2XhZj0c21jXzAMzFVVje9lZ2OvvGCG3OOIzv7HkcUZvEDBvgIBx-Hl1UbSjmcVqMJCBs8wLbx9vsAAQ']);
+            	curl_setopt($curl,CURLOPT_HEADER, false);
+            	curl_setopt($curl,CURLOPT_CUSTOMREQUEST, 'POST');
+            	curl_setopt($curl,CURLOPT_POSTFIELDS, $array250);
+            	curl_setopt($curl,CURLOPT_SSL_VERIFYPEER, 1);
+            	curl_setopt($curl,CURLOPT_SSL_VERIFYHOST, 2);
+            	$out = curl_exec($curl); //Инициируем запрос к API и сохраняем ответ в переменную
+            	curl_close($curl);
+            	echo $out;
+    		}
     }
 	
 
