c626d1b59a5ae79553dd23890332dde98caf7f8a
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-22 13:12:36 +0700

    Update amo_deals.php

--- amo_deals.php
+++ amo_deals.php
@@ -51,11 +51,11 @@ $data = array();
             	curl_setopt($curl,CURLOPT_SSL_VERIFYHOST, 2);
             	$out = curl_exec($curl); //Инициируем запрос к API и сохраняем ответ в переменную
             	curl_close($curl);
-            	$sum[] = json_decode($out, true)['id'];
+            	
     		
     
 
-    echo count(array_chunk($data, 200));
+    echo $out;
 	
 
 	
