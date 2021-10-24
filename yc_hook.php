014e7d1a3618b5d7002f1fb27f377311e7ba3e8c
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-24 21:29:40 +0700

    Update yc_hook.php

--- yc_hook.php
+++ yc_hook.php
@@ -28,11 +28,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 				break;
 			case 'update':
 				$clientData = $payload;
-				$amoId = $ycClass->getClientsDb(' where yc_id = ' . $resourseId);
-				$tableData = array('phone' => $clientData['data']['phone'], 'name' => $clientData['data']['name'], 'spent' => $clientData['data']['spent'], 'visits' => $clientData['data']['visits'], 'amo_id' => $amoId);
+
+				$amoId = $ycClass->getClientsDb(' where yc_id = ' . $resourseId)['amo_id'];
+				$tableData = array('phone' => $clientData['data']['phone'], 'name' => $clientData['data']['name'], 'spent' => $clientData['data']['spent'], 'visits' => $clientData['data']['visits']);
 				require_once 'amo_class.php'; //Класс для работы с API YCLIENTS
 				$amoClass = new AmoClass($company, 0); //В конструктор класса передаем название (название - поддомен компании из AMOCRM)
 				$result = $amoClass->setContact($tableData, $amoId);
+				$result .= ' ' . $ycClass->recordInDb('clients', 'yc_id', $resourceId, $tableData);
 				break;
 			case 'delete':
 				//Добавить удаление клиента из базы и из amocrm
@@ -41,7 +43,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 				$resultDb = '';
 				break;
 		}
-		$ycClass->recordHook($hookStatus);
+		$ycClass->recordHook($result);
    	}
    	else {
    		require_once 'ycClass.php'; //Класс для работы с API YCLIENTS
