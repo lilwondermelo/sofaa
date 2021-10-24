46a9c6ab1d6f716596a80b8b0f4df75817764eb0
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-24 21:54:32 +0700

    Update amo_class.php

--- amo_class.php
+++ amo_class.php
@@ -57,7 +57,6 @@ class AmoClass {
 		$type = 'POST';
 		$data = array();
 		$data[0]['name'] = $item['name'];
-
 		$data[0]['custom_fields_values'] = array(array("field_id" => $this->customFields['yc_id'], "values" => array(array("value" => $item['yc_id']))), array("field_id" => $this->customFields['phone'], "values" => array(array("value" => $item['phone']))), array("field_id" => $this->customFields['visits'], "values" => array(array("value" => $item['visits']))), array("field_id" => $this->customFields['spent'], "values" => array(array("value" => $item['spent']))));
 		$resId = $this->apiQuery($type, $link, $data)['_embedded']['contacts'][0]['id'];
 		return $resId;
