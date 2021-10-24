821910aa967a67ee5e72ed3ba099463c41fad443
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-24 18:08:07 +0700

    Update yc_hook.php

--- yc_hook.php
+++ yc_hook.php
@@ -3,7 +3,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $raw_payload = file_get_contents('php://input');
     require_once 'ycClass.php'; //Класс для работы с API YCLIENTS
 	$ycClass = new YCClass('data', 1); //В конструктор класса передаем название (название - поддомен компании из AMOCRM)
-	$hookType = $raw_payload['resource_type'];
+	$hookType = json_decode($raw_payload['resource_type'], true);
 	$data = array('data_value' => $hookType);
 	$result = $ycClass->recordInDb('sys', 'data_key', 'test_hook_' . date('Y-m-d H:i:s'), $data);
 	echo json_encode(json_decode($raw_payload, true));
