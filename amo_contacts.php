cf360f3bd2fe2db31dbeb2e35d66ff9091acd302
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-24 13:34:55 +0700

    Update amo_contacts.php

--- amo_contacts.php
+++ amo_contacts.php
@@ -20,7 +20,7 @@ if ($company != '') {
 	}
 	echo 'Компания: ' . $company . '<br>' . 'Тестовый режим ' . (($isTest == 1)?'ВКЛЮЧЕН':'не включен') . '<br>';
 	//echo json_encode($result_db);
-	echo json_encode($amoClass->getContacts());
+	echo $amoClass->getContacts();
 }
 else {
 	echo 'Компания не выбрана';
