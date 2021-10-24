96219a9dab5b86c87abd876bf6b012db14d36d46
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-24 23:19:12 +0700

    Update yc_hook.php

--- yc_hook.php
+++ yc_hook.php
@@ -22,7 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		
 
     if ($company != '') {
-    	$ycClass->recordHook('comp');
+    	$ycClass->recordHook($hookStatus);
 		switch ($hookStatus) {
 			case 'create':
 			$ycClass->recordHook('crt');
