2a71b2bab64d8e96841dba61cc7259943ea97ae2
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-25 00:08:06 +0700

    Update amo_class.php

--- amo_class.php
+++ amo_class.php
@@ -55,10 +55,10 @@ class AmoClass {
 	public function setContact($item, $amoId = '') {
 
 		$link='https://'.$this->host.'.amocrm.ru/api/v4/contacts/' . $amoId;
-$ycClass->recordHook($link);	
+	
 		require_once 'yc_class.php'; //Класс для работы с API YCLIENTS
 		$ycClass = new YCClass($this->host, 0); //В конструктор класса передаем название (название - поддомен компании из AMOCRM)
-		
+		$ycClass->recordHook($link);
 
 		$type = 'POST';
 		$data = array();
