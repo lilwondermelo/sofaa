26f2520eee6b247f16ee7f3c14d47ad0e5c1ebd5
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-25 13:57:53 +0700

    Update amo_dealhook.php

--- amo_dealhook.php
+++ amo_dealhook.php
@@ -15,13 +15,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$ycData['data']['visit_attendance'] = (int)$recordStatus;
 		$ycResult = $ycClass->editDeal($ycId, $ycData);
 		$resultDb = $ycClass->recordInDb('records', 'yc_id', $ycId, array('stat' => (int)$recordStatus));
-		$ycClass->recordHook(json_encode($ycData, JSON_UNESCAPED_UNICODE));
+		$ycClass->recordHook(json_encode($ycResult, JSON_UNESCAPED_UNICODE));
     }
     else {
     	$ycClass->recordHook(0);
     }
-    
-	
 
 }
 
