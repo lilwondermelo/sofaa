12af27ac08523a5f2e09e90a6cc19bf68d6a533a
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-25 12:12:33 +0700

    Update amo_hook.php

--- amo_hook.php
+++ amo_hook.php
@@ -3,7 +3,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $payload = json_decode(file_get_contents('php://input'), true);
     require_once 'yc_class.php'; //Класс для работы с API YCLIENTS
 	$ycClass = new YCClass('data', 0); //В конструктор класса передаем название (название - поддомен компании из AMOCRM)
-	$ycClass->recordHook(fixedEncodeURI(file_get_contents('php://input')));
+	$ycClass->recordHook($_POST);
 }
 
 
