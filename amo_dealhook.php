e6d085468de968942e9a65c958fc277871098c1a
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-25 13:45:26 +0700

    Update amo_dealhook.php

--- amo_dealhook.php
+++ amo_dealhook.php
@@ -7,9 +7,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     	require_once 'yc_class.php'; //Класс для работы с API YCLIENTS
 		$ycClass = new YCClass($company, 0); //В конструктор класса передаем название (название - поддомен компании из AMOCRM)
 		$recordStatus = $ycClass->getStatus($statusId);
-		$ycData = array();
-		$ycData['attendance'] = (int)$recordStatus;
+		
+		
 		$ycId = $ycClass->getDealsDb(' where amo_id = ' . $recordId)[0]['yc_id'];
+		$ycData = $ycClass->getRecord($ycId);
+		$ycData['attendance'] = (int)$recordStatus;
 		$ycResult = $ycClass->editDeal($ycId, $ycData);
 		$resultDb = $ycClass->recordInDb('records', 'yc_id', $ycId, array('stat' => (int)$recordStatus));
 		$ycClass->recordHook($resultDb);
