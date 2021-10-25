abb5148c3abab21e0f58e097e022af487c9df98c
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-25 09:06:07 +0700

    Update amo_class.php

--- amo_class.php
+++ amo_class.php
@@ -92,7 +92,6 @@ class AmoClass {
 
 
 	public function setDeals($data) {
-		return json_encode($data[0]);
 		$link='https://'.$this->host.'.amocrm.ru/api/v4/leads';
 		$type = 'POST';
 		$result = $this->apiQuery($type, $link, $data)['_embedded']['leads'];
@@ -105,7 +104,7 @@ class AmoClass {
 			$resultDb[] = $ycClass->recordInDb('records', 'yc_id', $data[$i]['custom_fields_values'][0]['values'][0]['value'], array('amo_id' => $resId));
 			$i++;
 		}
-		//return json_encode($resultDb) . json_encode($data[0]);
+		return json_encode($resultDb));
 	}
 
 	public function getContactsDB() {
