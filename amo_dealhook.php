dd70380a34ac7232f488e1d776f9b1cc5185cfa0
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-25 13:37:41 +0700

    Update amo_dealhook.php

--- amo_dealhook.php
+++ amo_dealhook.php
@@ -3,7 +3,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$recordId = $_POST['leads']['update'][0]['id'];
     $company = $_POST['account']['subdomain'];
     $statusId = $_POST['leads']['update'][0]['status_id'];
-    if ($status_id > 0) {
+    if ($statusId > 0) {
     	require_once 'yc_class.php'; //Класс для работы с API YCLIENTS
 		$ycClass = new YCClass($company, 0); //В конструктор класса передаем название (название - поддомен компании из AMOCRM)
 		$recordStatus = $ycClass->getStatus($statusId);
