ee1c95fd39f7780a781ef1e60e5b82f8f10c8eac
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-27 09:31:50 +0700

    Update yc_clients.php

--- yc_clients.php
+++ yc_clients.php
@@ -13,7 +13,8 @@ if ($company != '') {
 	$resultDb = array(); //Массив для занесения результатов добавления данных в БД
 	require_once 'yc_class.php'; //Класс для работы с API YCLIENTS
 	$ycClass = new YCClass($company, $isTest); //В конструктор класса передаем название (название - поддомен компании из AMOCRM)
-	$pages = ($isTest == 1)?1:((ceil($ycClass->getCLientCount()['pages']) > 5)?5:ceil($ycClass->getCLientCount()['pages'])); //Количество страниц в запросе пользователей;
+	$pages = 1;
+	//$pages = ($isTest == 1)?1:((ceil($ycClass->getCLientCount()['pages']) > 5)?5:ceil($ycClass->getCLientCount()['pages'])); //Количество страниц в запросе пользователей;
 	for ($i = $page*5-5; $i < $page*5-5+$pages; $i++) { //цикл перебирает страницы (API YCLIENTS не дает больше 200 значений на одну страницу)
 		$pageData = $ycClass->getClients($i+1); //$i+1 - номер текущей страницы
 		foreach ($pageData['data'] as $item) {
@@ -22,7 +23,7 @@ if ($company != '') {
 			$result_db[] = $ycClass->recordInDb('clients', 'yc_id', $item['id'], $tableData);
 		}
 	}
-	echo 'Компания: ' . $company . '<br>' . 'Тестовый режим ' . (($isTest == 1)?'ВКЛЮЧЕН':'не включен') . '<br>';
+	echo 'Компания: ' . $company . '<br>' . 'Тестовый режим ' . (($isTest == 1)?'ВКЛЮЧЕН':'не включен') . '<br>' ;
 	echo json_encode($result_db);
 }
 else {
