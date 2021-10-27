2d5afe2465a807b97001dc0648ecc48902c77c0c
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-27 15:33:31 +0700

    123

--- amo_dealhook.php
+++ amo_dealhook.php
@@ -1,14 +1,17 @@
 <?php 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	$recordId = $_POST['leads']['update'][0]['id'];
+	$company = $_POST['account']['subdomain'];
+	require_once 'yc_class.php'; //Класс для работы с API YCLIENTS
+	$ycClass = new YCClass($company, 0); //В конструктор класса передаем название (
+	$ycClass->recordHook(json_encode($_POST));
+	/*$recordId = $_POST['leads']['update'][0]['id'];
     $company = $_POST['account']['subdomain'];
     $statusId = $_POST['leads']['update'][0]['status_id'];
     if ($statusId > 0) {
-    	require_once 'yc_class.php'; //Класс для работы с API YCLIENTS
-		$ycClass = new YCClass($company, 0); //В конструктор класса передаем название (название - поддомен компании из AMOCRM)
+    	название - поддомен компании из AMOCRM)
 		$recordStatus = $ycClass->getStatus($statusId);
-		
-		
+		require_once 'yc_class.php'; //Класс для работы с API YCLIENTS
+		$ycClass = new YCClass($company, 0); //В конструктор класса передаем название (
 		$ycId = $ycClass->getDealsDb(' where amo_id = ' . $recordId)[0]['yc_id'];
 		$ycData = $ycClass->getRecord($ycId)['data'];
 		if ($ycData['attendance'] != (int)$recordStatus) {
@@ -16,12 +19,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			$ycData['visit_attendance'] = (int)$recordStatus;
 			$ycResult = $ycClass->editDeal($ycId, $ycData);
 			$resultDb = $ycClass->recordInDb('records', 'yc_id', $ycId, array('stat' => (int)$recordStatus));
-			$ycClass->recordHook(json_encode($ycResult, JSON_UNESCAPED_UNICODE));
+			$ycClass->recordHook(json_encode($ycResult, JSON_UNESCAPED_UNICODE));*/
 		}
 		
     }
     else {
-    	$ycClass->recordHook(0);
+    	//$ycClass->recordHook(0);
     }
 
 }
