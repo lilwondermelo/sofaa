0cfbc086ab915fded048970fbe8ce696d344956c
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-28 15:38:48 +0700

    Update yc_hook.php

--- yc_hook.php
+++ yc_hook.php
@@ -23,7 +23,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 				$amoData = $contact->convertToAmo();
 				$amoId = $controller->checkAmoContact($contact);
 				$resId = $controller->setContactToAmo($amoData, $amoId);
-				echo $account->getAmoBearer();
+				echo 1;
 				break;
 			case 'delete':
 				//Добавить удаление клиента из базы и из amocrm
