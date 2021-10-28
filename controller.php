f65ef79532eac5ee64519a96c4764f9bd925bdb4
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-28 14:29:18 +0700

    Update controller.php

--- controller.php
+++ controller.php
@@ -50,8 +50,10 @@ class Controller {
 		$this->link = 'https://' . $this->account->getAmoHost() . '.amocrm.ru/api/v4/contacts';
 		$this->method = 'GET';
 		$filter = [
-			$this->account->getCustomFields()['yc_id'] => $contact->getId(),
-			$this->account->getCustomFields()['phone'] => $contact->getPhone()
+			'filter' => [
+				$this->account->getCustomFields()['yc_id'] => $contact->getId(),
+				$this->account->getCustomFields()['phone'] => $contact->getPhone()
+			]
 		];
 		$result = $this->apiQuery($filter);
 		/*$resId = $result['_embedded']['contacts'][0]['id'];
