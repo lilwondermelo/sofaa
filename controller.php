46b64d591b6882a1d4e7d5f2c3a060c98c1debc1
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-28 18:03:33 +0700

    Update controller.php

--- controller.php
+++ controller.php
@@ -69,7 +69,7 @@ class Controller {
 		if (!$resId) {
 			return -1;
 		}
-		return json_encode($filter);
+		return http_build_query($filter);
 	}
 
 	public function setContactToAmo($contact, $amoId = -1) {
