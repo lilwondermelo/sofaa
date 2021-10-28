c4f92b6a9d6ce93f99551fa8d79dc1917c7cdf65
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-28 16:15:08 +0700

    Update controller.php

--- controller.php
+++ controller.php
@@ -38,12 +38,12 @@ class Controller {
 		curl_setopt($curl,CURLOPT_SSL_VERIFYPEER, 1);
 		curl_setopt($curl,CURLOPT_SSL_VERIFYHOST, 2);
 		$out=curl_exec($curl);
-		//$code=curl_getinfo($curl,CURLINFO_HTTP_CODE);
+		$code=curl_getinfo($curl,CURLINFO_HTTP_CODE);
 		//Добавить обновление Bearer при ощибке авторизации!!!
 		curl_close($curl);
 		$result = json_decode($out, true);
 		//return $result;
-		return $out;
+		return $code;
 	}
 
 	public function checkAmoContact($contact) {
