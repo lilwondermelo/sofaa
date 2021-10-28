9b3cefa003d30bdd13c0b6013d44b537941033bf
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-28 14:26:38 +0700

    Update controller.php

--- controller.php
+++ controller.php
@@ -42,7 +42,7 @@ class Controller {
 		curl_close($curl);
 		$result = json_decode($out, true);
 		//return $result;
-		return json_encode($args);
+		return json_encode($this->link);
 	}
 
 	public function checkAmoContact($contact) {
