7499def5cea5423046ed2b8e054bb7f3c46cca81
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-29 11:05:30 +0700

    Update yc_clients.php

--- yc_clients.php
+++ yc_clients.php
@@ -18,14 +18,14 @@ if ($company != '') {
 	$pages = (ceil($clientList['pages']) > 5)?5:ceil($clientList['pages']);
 	for ($i = $page*5-5; $i < $page*5-5+$pages; $i++) { //цикл перебирает страницы (API YCLIENTS не дает больше 200 значений на одну страницу)
 		$pageData = $controller->getClientList($i+1); //$i+1 - номер текущей страницы
-		foreach ($pageData['data'] as $item) {
-			$clientData = $controller->getClientData($item['id']);
-			$amoRequestData[] = $clientData;
-		}
+		//foreach ($pageData['data'] as $item) {
+			//$clientData = $controller->getClientData($item['id']);
+			//$amoRequestData[] = $clientData;
+		//}
 	}
 	
 	echo 'Компания: ' . $company . '<br>';
-	echo json_encode($amoRequestData[0]);
+	echo json_encode($pageData);
 }
 else {
 	echo 'Компания не выбрана';
