2d4ae1b98d86d9474eeb32a341c6028679679f97
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-29 16:56:44 +0700

    Update amo_contacthook.php

--- amo_contacthook.php
+++ amo_contacthook.php
@@ -1,6 +1,6 @@
 <?php 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	$contactData = $_POST['contacts']['update'][0]['id'];
+	$contactData = $_POST;
 	//$recordId = $_POST['leads']['update'][0]['id'];
 	echo json_encode($contactData);
 	echo 1;
