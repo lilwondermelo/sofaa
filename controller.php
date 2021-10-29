e56c92be72bea9455a5a3188188582ae04777ff2
Al Mikhtadov <lilwondermelo@gmail.com>
2021-10-29 09:54:01 +0700

    Update controller.php

--- controller.php
+++ controller.php
@@ -109,7 +109,7 @@ class Controller {
 		$clientCount = $result['meta']['total_count'];
 		$pagesCount = $clientCount/$this->dataPerPage;
 		//return array('clients' => $clientCount, 'pages' => $pagesCount);
-		return $this->authHeader;
+		return $this->link;
 	}
 
 	public function setContactToAmo($contact, $amoId = -1) {
