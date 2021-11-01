ef937a6439e9576b6d280722846c0965b66b70f6
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-01 12:13:29 +0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -97,7 +97,7 @@ Class Contact {
 		$this->setPhone($phone);
 		//Проверка на соответствие имени клиента в YC и контакта в AMO !!!
 		$this->setName($this->contactData['name']);
-		return array_search($this->customFields['phone]'], $dataCustomFieldIds);
+		return $this->customFields['phone]';
 	}
 
 
