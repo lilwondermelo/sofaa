e01fa70ee77fb7dc96ab4927885f3d135d7c027f
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-01 09:25:04 +0700

    123

--- contact.php
+++ contact.php
@@ -75,7 +75,7 @@ Class Contact {
 	public function createFromAmo() {
 		$dataCustomFieldIds = array_column($this->contactData['custom_fields'], 'id'); //Создает массив из значений id
 		$indexPhone = array_search($this->customFields['phone]'], $dataCustomFieldIds); //Ищет по созданному массиву индекс, в котором содержится нужный телефон
-		Проверку телефона добавить !!!
+		//Проверку телефона добавить !!!
 		$phone = $this->contactData['custom_fields'][$indexPhone]['values'][0]['value'];
 		//$phone = '+79999999999';
 		$this->setPhone($phone);
