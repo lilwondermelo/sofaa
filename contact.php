1cc1133a184de054447f013e7c82f03ad00d3457
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-01 09:14:29 +0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -73,24 +73,15 @@ Class Contact {
 	}
 
 	public function createFromAmo() {
-		$dataCustomFieldIds = array_column($this->contactData['custom_fields'], 'id'); //Создает массив из значений id
-		if ($dataCustomFieldIds) {
-			$indexPhone = array_search($this->customFields['phone]'], $dataCustomFieldIds); //Ищет по созданному массиву индекс, в котором содержится нужный телефон
-			if ($indexPhone) {
-				//Проверку телефона добавить !!!
-				$phone = $this->contactData['custom_fields'][$indexPhone]['values'][0]['value'];
-			}
-			else {
-				$phone = +79999999999;
-			}
-		}
-		else {
-			$phone = +79999999999;
-		}
+		//$dataCustomFieldIds = array_column($this->contactData['custom_fields'], 'id'); //Создает массив из значений id
+		//$indexPhone = array_search($this->customFields['phone]'], $dataCustomFieldIds); //Ищет по созданному массиву индекс, в котором содержится нужный телефон
+		//Проверку телефона добавить !!!
+		//$phone = $this->contactData['custom_fields'][$indexPhone]['values'][0]['value'];
+		$phone = '+79999999999';
 		$this->setPhone($phone);
 		//Проверка на соответствие имени клиента в YC и контакта в AMO !!!
 		$this->setName($this->contactData['name']);
-		return $this->getName();
+		return $this->contactData['name'];
 	}
 
 	public function editFromAmo() {
