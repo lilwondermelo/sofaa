db774cab179de2024282fd6e53dbbac2ae5ea750
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-01 20:46:42 +0700

    123

--- amo_contacthook.php
+++ amo_contacthook.php
@@ -24,9 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$kek = $contact->editFromAmo();
 		$amoData = $contact->convertToYC();
 		$result = $controller->setContactToYC($amoData);
-
 		$controller->recordHook(json_encode($result, JSON_UNESCAPED_UNICODE));
-		
 		echo json_encode($entityType, JSON_UNESCAPED_UNICODE);
 	}
 	
