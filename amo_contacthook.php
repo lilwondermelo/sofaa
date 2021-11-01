c9ea061da7cbf4f77c499bf8027d4894436e6952
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-01 08:20:52 +0700

    Update amo_contacthook.php

--- amo_contacthook.php
+++ amo_contacthook.php
@@ -26,7 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$amoData = $contact->convertToYC();
 	$result = $controller->setContactToYC($amoData);
 
-	$controller->recordHook(json_encode($amoHost));
+	$controller->recordHook(json_encode($entityData));
 	echo json_encode($result, JSON_UNESCAPED_UNICODE);
 }
 
