f291725a929dde3cf3586eff1b013f8fbc58ef1f
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-01 13:53:26 +0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -76,8 +76,8 @@ Class Contact {
 		//$dataCustomFieldIds = array_column($this->contactData['custom_fields'], 'id'); //Создает массив из значений id
 		//$indexPhone = array_search($this->customFields['phone]'], $dataCustomFieldIds); //Ищет по созданному массиву индекс, в котором содержится нужный телефон
 		//Проверку телефона добавить !!!
-		//$phone = $this->contactData['custom_fields'][$indexPhone]['values'][0]['value'];
-		$phone = '+79999999999';
+		$phone = $this->contactData['custom_fields'][$indexPhone]['values'][0]['value'];
+		//$phone = '+79999999999';
 		$this->setPhone($phone);
 		//Проверка на соответствие имени клиента в YC и контакта в AMO !!!
 		$this->setName($this->contactData['name']);
