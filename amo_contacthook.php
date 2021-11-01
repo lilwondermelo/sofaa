c80aaf3e22da319e317b12b72bf8414ce5a8ebf1
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-01 15:03:44 +0700

    123

--- amo_contacthook.php
+++ amo_contacthook.php
@@ -18,7 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$amoData = $contact->convertToAmo();
 		$amoId = $entityData['id'];
 		$resId = $controller->setContactToAmo($amoData, $amoId);
-		$controller->recordHook(json_encode($resId, JSON_UNESCAPED_UNICODE));
+		$controller->recordHook(json_encode($amoId, JSON_UNESCAPED_UNICODE));
 	}
 	else if ($actionType == 'update') {
 		$kek = $contact->editFromAmo();
