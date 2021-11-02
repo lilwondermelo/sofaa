f2d2d799ac59004dc6ada0dd8c89e54b5ff31995
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-02 13:57:24 +0700

    Update yc_deals.php

--- yc_deals.php
+++ yc_deals.php
@@ -21,35 +21,33 @@ if ($company != '') {
 		$amoDealsData = [];
 		foreach ($pageData['data'] as $item) {
 			$clientData = $controller->getClientData($item['id']);
-			$dealData = $controller->getLastClientRecord($item['id'])['data'];
+			$dealData = $controller->getLastClientRecord($item['id']);
 			$stat = $dealData['visit_attendance'];
 			$data[] = array(
-			'custom_fields_values' => array(array("field_id" => $account->getCustomFields()['deal_yc_id'], "values" => array(array("value" => '' . $dealData['id']))), array("field_id" => $account->getCustomFields()['deal_date'], "values" => array(array("value" => $dealData['date'])))),
-			'name' => 'Запись из YCLIENTS',
-			'price' => 1,
-			'status_id' => $account->getStatuses()[$stat]
-			
-		);
+				'custom_fields_values' => array(array("field_id" => $account->getCustomFields()['deal_yc_id'], "values" => array(array("value" => '' . $dealData['id']))), array("field_id" => $account->getCustomFields()['deal_date'], "values" => array(array("value" => $dealData['date'])))),
+				'name' => 'Запись из YCLIENTS',
+				'price' => 1,
+				'status_id' => $account->getStatuses()[$stat]
+			);
 			$amoRequestData[] = $clientData;
-			$amoDealsData[] = $data;
+			$amoDealsData[] = $dealData;
 
 		}
-		$result[] = $controller->setManyContactsToAmo($amoRequestData);	
+		$result = $controller->setManyContactsToAmo($amoRequestData);
+		$counter = 0;
+		foreach ($result['_embedded']['contacts'] as $contact)  {
+			$amoId = $contact['id'];
+			$data[$counter]['_embedded'] = array('contacts' => array(array('id' => $amoId)));
+			$counter++;
+		}
+		echo json_encode($result) . '<br><br>';
+		echo json_encode($amoDealsData);
 		//$result[] = $controller->setManyDealsToAmo($amoDealsData);	
 		//echo json_encode($amoDealsData) . '<br><br>';
 	}
 	
-	echo json_encode($result) . '<br><br>';
-	$counter = 0;
-	foreach ($result as $item) {
-		foreach ($item['_embedded']['contacts'] as $contact)  {
-			$amoId = $contact['id'];
-			$data[$i]['_embedded'] = array('contacts' => array(array('id' => $amoId)));
-			$i++;
-		}
-	}
+	
 	//$result = $controller->setManyDealsToAmo($data);
-	echo json_encode($data);
 	echo 'Компания: ' . $company . '<br>';
 }
 else {
