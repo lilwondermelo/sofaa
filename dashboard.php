2f353c793c48f37633ade115e544e768b87ddf7b
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-02 16:12:38 +0700

    123

---
+++ dashboard.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>AutoBeauty Dashboard</title>
+	<script src="js/jquery-3.5.1.js"></script>
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+</head>
+<body>
+	
+	<script src="js/main.js"></script>
+</body>
+</html>
