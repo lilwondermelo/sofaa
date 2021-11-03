2c5f3cc70110ad385984dd0189149770f543940b
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-03 17:23:19 +0700

    Update controller.php

--- controller.php
+++ controller.php
@@ -5,7 +5,7 @@ class Controller {
 	private $link;
 	private $method;
 	private $authHeader;
-	private $dataPerPage = 100;
+	private $dataPerPage = 50;
 
 	public function __construct($account){
 		$this->account = $account;
