00b77e826e96a08a31bee233d8c9be5aa72a979a
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-03 18:46:16 +0700

    Update yc_deals.php

--- yc_deals.php
+++ yc_deals.php
@@ -62,6 +62,10 @@ if ($company != '') {
 	
 	
 	//$result = $controller->setManyDealsToAmo($data);
+	foreach (headers_list() as $header) {
+		header_remove($header);
+	}
+    
 	echo 'Компания: ' . $company . '<br>';
 	header('Location: ingeniouslife.space/yc_deals.php?company=jkamogolovaorg&page=' . ($page+1));
 }
