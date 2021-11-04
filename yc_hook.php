b4162ef4c41fc492e0c2f0a901326b4029e4b6cc
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-04 11:58:23 +0700

    Update yc_hook.php

--- yc_hook.php
+++ yc_hook.php
@@ -22,7 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 				//$controller->recordHook(json_encode($amoData, JSON_UNESCAPED_UNICODE) . '3');
 				$amoId = $controller->checkAmoContact($contact);
 				//$controller->recordHook('result' . json_encode($amoId, JSON_UNESCAPED_UNICODE));
-				$resId = $controller->setContactToAmo($amoData, $amoId);
+				//$resId = $controller->setContactToAmo($amoData, $amoId);
 				echo 'result' . json_encode($amoId, JSON_UNESCAPED_UNICODE);
 		}
 	}
