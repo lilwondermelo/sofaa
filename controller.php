f62230ba184727285aade0ae6f1343e51950da3b
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-08 19:52:28 +0700

    Update controller.php

--- controller.php
+++ controller.php
@@ -223,7 +223,7 @@ class Controller {
 		$result = $this->apiQuery($contact);
 		$resId = $result['data']['id'];
 		if (!$resId) {
-			return false;
+			return $contact['id'];
 		}
 		return $resId;
 	}
