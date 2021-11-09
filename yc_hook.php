70a9c081860482ad5b714bffd7828dd7d7b7dc0b
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-09 23:54:13 +0700

    Update yc_hook.php

--- yc_hook.php
+++ yc_hook.php
@@ -60,59 +60,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		}
 	}
 
-
-
-
-	/*else {
+	else {
 		sleep(2);
-		$clientId = $contactData['client']['id'];
-		$amoContactData = $controller->getAmoContact($clientId);
-		$amoId = $amoContactData['_embedded']['contacts'][0]['id'];
-		$amoDeal = $amoContactData['_embedded']['contacts'][0]['_embedded']['leads'][0];
-		$stat = $contactData['visit_attendance'];
-		$data = array(
-			'custom_fields_values' => array(array("field_id" => $account->getCustomFields()['deal_yc_id'], "values" => array(array("value" => '' . $contactData['id']))), array("field_id" => $account->getCustomFields()['deal_datetime'], "values" => array(array("value" => strtotime($contactData['date']))))),
-			'name' => 'Запись из YCLIENTS',
-			'price' => 1,
-			'status_id' => $account->getStatuses()[$stat],
-			'_embedded' => array('contacts' => array(array('id' => $amoId)))
-		);
-
-		if (!$amoDeal) {
-			$result = $controller->setDealToAmo($data);
-		}
-		else {
-			$data['id'] = $amoDeal['id'];
-			$result = $controller->setDealToAmo($data, '1');
-		}
-		$controller->recordHook(json_encode($result, JSON_UNESCAPED_UNICODE));
+		$controller->recordHook('333' . json_encode($contactData, JSON_UNESCAPED_UNICODE));
+		
 	}
-
-	else if ($hookType == 'record') {
-		switch ($hookStatus) {
-			case 'create':
-			$recordData = $payload;
-			$ycClass->recordHook(json_encode($recordData));
-				break;
-			case 'update':
-				$amoData = array();
-				$recordData = $payload;
-				$amoId = $ycClass->getDealsDb(' where yc_id = ' . $resourceId)[0]['amo_id'];
-				$stat = ($recordData['data']['visit_attendance'])?$recordData['data']['visit_attendance']:'0';
-				$amoData[0] = array(
-					'status_id' => $amoClass->getStatus($stat)
-				);
-				$result = $amoClass->setDeals($amoData, $amoId);
-				$result .= ' ' . $ycClass->recordInDb('records', 'yc_id', $resourceId, array('stat' => $stat));
-				break;
-			case 'delete':
-				//Добавить удаление клиента из базы и из amocrm
-				break;
-			default:
-				$resultDb = '';
-				break;
-		}
-    }*/
 }  
 ?>
 
