cff48e0527642a554ade225e43d00aa92cd56785
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-09 20:43:49 +0700

    Update yc_hook.php

--- yc_hook.php
+++ yc_hook.php
@@ -30,11 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			if ($resultDb) {
 				$contact->setAmoId($amoId);
 				$amoData = $contact->convertToAmo();
-
-
 				
-
-
 				//$controller->recordHook('222' . json_encode($resAmoArray, JSON_UNESCAPED_UNICODE));
 				if (($amoId != -1) && ($leadId != -1)) {
 					$resAmo = $controller->setContactToAmo($amoData, $amoId);
@@ -48,7 +44,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			else {
 				$resAmo = false;
 			}
-			$controller->recordHook($resAmo);
+			$controller->recordHook($resAmoArray);
 			if ($resAmo) {
 				$contact->setAmoId($resAmo);
 				$result = $controller->recordContactFromAmo($contact, $contact->getId());
