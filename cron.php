fa2a5e60771e138bd01b4b5cb0b7222f85482009
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-10 16:04:31 +0700

    Create cron.php

---
+++ cron.php
@@ -0,0 +1,19 @@
+<?php 
+
+
+
+	function recordHook($data = 'empty') {
+		require_once '_dataRowUpdater.class.php';
+		$updater = new DataRowUpdater('sys_data');
+		$updater->setKeyField('id');
+		$updater->setDataFields(array('data_key' => 'test_hook_' . date('Y-m-d H:i:s'), 'data_value' => 'crontetst'));
+		$result_upd = $updater->update();
+		if (!$result_upd) {
+			return $updater->error;
+		}
+		else {
+			return $result_upd;
+		}
+	}
+
+?>
