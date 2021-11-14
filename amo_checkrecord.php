3fcb1cdec31c30bb3af1653efc55e26bf0e802a9
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-14 21:23:19 +0700

    123

--- amo_checkrecord.php
+++ amo_checkrecord.php
@@ -1,13 +1,22 @@
 <?php 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
+//$postData = json_decode(file_get_contents('php://input'), true);
 	$postData = $_POST;
+	$entityType = array_key_first($postData);
+	$amoHost = $postData['account']['subdomain'];
+	$actionType = array_key_first($postData[$entityType]);
+	$entityData = $postData[$entityType][$actionType][0];
+
 	require_once 'account.php';
 	$account = new Account($amoHost);
 	require_once 'controller.php';
 	$controller = new Controller($account);
-	$active = $controller->getLastRecord($contactData['client']['id']);
-	$result = $controller->setRecordToAmo($active);
+
+	require_once 'contact.php';
+	$contact = new Contact($entityData, $account->getCustomFields());
+
+	$active = $controller->getLastRecord($contact->getId());
+			$result = $controller->setRecordToAmo($active);
 	$controller->recordHook('1 '. json_encode($postData, JSON_UNESCAPED_UNICODE));
 }
 ?>
