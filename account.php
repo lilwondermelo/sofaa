187fee454d940ef4e95bf8c98cd6fefff758965d
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-16 02:13:06 +0700

    Update account.php

--- account.php
+++ account.php
@@ -60,7 +60,7 @@ Class Account {
 		$result_upd = $updater->update();
 
 		if (!$result_upd) {
-			return json_encode($requestData);
+			return $updater->error;
 		}
 		else {
 			$this->setAmoBearer($decodedResponse['access_token']);
