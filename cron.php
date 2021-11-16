3043b45551430e0fbe758055287a3a277cf99a61
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-16 12:51:41 +0700

    Update cron.php

--- cron.php
+++ cron.php
@@ -21,12 +21,9 @@ and r.`24h` = 0 and attendance != -1';
 				);
 
 		$result = $controller->setRequestToAmo([$dataReq]);
-		echo json_encode($dataReq, JSON_UNESCAPED_UNICODE);
+		echo json_encode($result, JSON_UNESCAPED_UNICODE);
 		}
 	}
-
-
-echo strtotime(date('Y-m-d H:i:s') . '+1 day');
 	
 
 ?>
