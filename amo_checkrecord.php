37b2f74c4c227e314c0880b8990452a8cc568aff
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-17 14:50:56 +0700

    Update amo_checkrecord.php

--- amo_checkrecord.php
+++ amo_checkrecord.php
@@ -4,13 +4,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$postData = $_POST;
 
 	$amoHost = $postData['account']['subdomain'];
-
+	$entityType = array_key_first($postData['leads']);
+	
 	require_once 'account.php';
 	$account = new Account($amoHost);
 	require_once 'controller.php';
 	$controller = new Controller($account);
 
-	$active = $controller->getLastRecordByAmo($postData['leads']['add'][0]['id']);
+	$active = $controller->getLastRecordByAmo($postData['leads'][$entityType][0]['id']);
 	$result = $controller->setRecordToAmo($active);
 
 	$controller->recordHook('1 '. json_encode($postData, JSON_UNESCAPED_UNICODE));
