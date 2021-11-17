d618af8154b872536ab41906cc6d9ded79839737
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-17 14:01:15 +0700

    123

--- cron.php
+++ cron.php
@@ -66,8 +66,8 @@ and c.lead_id is not null';
 require_once '_dataSource.class.php';
 	$query24 = 'select c.lead_id as leadId, c.amo_host as amoHost, r.record_id as recordId from records r 
 join clients c on r.client_id = c.yc_id
-and r.datetime <= ' . strtotime(date('Y-m-d H:i:s') . '-1 hour') . '
-and r.req = 0 and attendance != -1 
+and r.datetime <= ' . strtotime(date('Y-m-d H:i:s') . '-1 day') . '
+and r.req = 0 and attendance = 1 
 and c.lead_id is not null';
 	$dataSource = new DataSource($query24);
 	$data = $dataSource->getData();
