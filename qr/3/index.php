51c5cceda9c4d53dc0d7df14cab7337ad0fb6e5c
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-17 23:40:34 +0700

    Update index.php

--- qr/3/index.php
+++ qr/3/index.php
@@ -1,4 +1,4 @@
 <?php 
-header('Location: https://static.wikia.nocookie.net/flex-air/images/6/6c/Ricardo.png/revision/latest/top-crop/width/360/height/450?cb=20201123170212&path-prefix=ru');
+header('Location: https://c.tenor.com/tVrkM5XhW-EAAAAM/flick-esfand.gif');
 
  ?>
