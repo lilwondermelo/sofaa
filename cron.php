5d71238a3dbfcbe0824a7df7d368b6bb1fd62041
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-17 16:38:43 +0700

    Update cron.php

--- cron.php
+++ cron.php
@@ -1,6 +1,6 @@
 <?php 
 require_once '_dataSource.class.php';
-	$query24 = 'select r.datetime as dateTime, c.lead_id as leadId, c.amo_host as amoHost, r.record_id as recordId from records r 
+	$query24 = 'select r.record_id as recordId, r.datetime as dateTime, c.lead_id as leadId, c.amo_host as amoHost, r.record_id as recordId from records r 
 join clients c on r.client_id = c.yc_id
 and r.datetime >= ' . strtotime(date('Y-m-d H:i:s')) . ' 
 and r.creating = 0 and attendance != -1 
