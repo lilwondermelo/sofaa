c9dbcd792edea5a5f3b2073c5406fb3d58c4b4fe
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-22 11:28:42 +0700

    Update yc_records.php

--- yc_records.php
+++ yc_records.php
@@ -86,7 +86,7 @@ if ($company != '') {
 			$active = $controller->getLastRecord($item['id']);
 
 			$result = $controller->setRecordToAmo($active);
-			echo json_encode($contactData['client']['id'], JSON_UNESCAPED_UNICODE);
+			echo json_encode($active, JSON_UNESCAPED_UNICODE);
 			echo json_encode($resultDb, JSON_UNESCAPED_UNICODE);
 		}
 	}
