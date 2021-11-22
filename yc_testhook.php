bf7e504906233156ced9da587588a81993c10213
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-22 11:47:00 +0700

    Create yc_testhook.php

---
+++ yc_testhook.php
@@ -0,0 +1,16 @@
+<?php 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+	$postData = json_decode(file_get_contents('php://input'), true);
+
+	$contactData = $postData['data'];
+	$hookType = $postData['resource'];
+	$hookStatus = $postData['status'];
+	$companyId = $postData['company_id'];
+	require_once 'account.php';
+	$account = new Account($companyId);
+	require_once 'controller.php';
+	$controller = new Controller($account);
+	echo json_encode($postData, JSON_UNESCAPED_UNICODE);
+}
+?>
