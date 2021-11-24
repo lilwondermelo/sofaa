45e0a6282791405e88aa98e87392473f1d4a9efa
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-24 23:41:41 +0700

    Update amo_confirm.php

--- amo_confirm.php
+++ amo_confirm.php
@@ -1,42 +1,42 @@
 <?php 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-//$postData = json_decode(file_get_contents('php://input'), true);
-	$postData = $_POST;
+$postData = json_decode(file_get_contents('php://input'), true);
+	//$postData = $_POST;
 
 	$amoHost = $postData['account']['subdomain'];
 	$entityType = array_key_first($postData['leads']);
 	$leadId = $postData['leads'][$entityType][0]['id'];
 	
-	require_once 'account.php';
-	$account = new Account($amoHost);
-	require_once 'controller.php';
-	$controller = new Controller($account);
-
-
-
-$query = 'select r.datetime as dateTime, c.lead_id as leadId, c.amo_host as amoHost, r.record_id as recordId, r.client_id as clientId from records r 
-join clients c on r.client_id = c.yc_id
+$query = '
+select * from clients_yc yc 
+join records r 
+on yc.yc_id = r.client_id 
+where yc.lead_id = ' . $leadId . '
 and r.datetime < ' . strtotime(date('Y-m-d') . '+2 days') . ' 
-and r.datetime >= ' . strtotime(date('Y-m-d H:i:s')) . ' 
-and c.lead_id = ' . $leadId . ' 
-and r.deleted = 0 
-and c.lead_id is not null';
+and r.datetime >= ' . strtotime(date('Y-m-d H:i:s')) . '
+and r.deleted = 0';
 
 require_once '_dataSource.class.php';
 $dataSource = new DataSource($query);
 if ($data = $dataSource->getData()) {
 	foreach ($data as $item) {
 		$recordId = $item['recordId'];
+
+		require_once 'account.php';
+		$account = new Account($item['filial_id'], 'yc');
+		require_once 'controller.php';
+		$controller = new Controller($account);
 		$recData = $controller->getRecord($recordId);
 		$recData['attendance'] = 2;
 		$recData['visit_attendance'] = 2;
 		$recData['confirmed'] = 1;
-		//$rez[] = $controller->confirmRecordToYC($recordId, $recData);
+		$rez[] = $controller->confirmRecordToYC($recordId, $recData);
 
 	}
 }
-	//$active = $controller->getLastRecordByAmo($leadId);
-	//$result = $controller->setRecordToAmo($active);
-	//$controller->recordHook('confirm '. json_encode($rez, JSON_UNESCAPED_UNICODE));
+	$active = $controller->getLastRecordByAmo($leadId);
+	$result = $controller->setRecordToAmo($active);
+	$controller->recordHook('confirm '. json_encode($postData, JSON_UNESCAPED_UNICODE));
+	
 }
 ?>
