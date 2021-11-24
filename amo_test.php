bd8bc31f3bcb933fe4da22e564806f5502829896
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-24 11:51:10 +0700

    Update amo_test.php

--- amo_test.php
+++ amo_test.php
@@ -6,30 +6,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$amoHost = $postData['account']['subdomain'];
 	$actionType = array_key_first($postData[$entityType]);
 	$entityData = $postData[$entityType][$actionType][0];
-
-
-	$leadId = $entityData['linked_leads_id'];
-
-	if (is_array($leadId)) {
-		$leadId = array_key_first($leadId);
-	}
-	else {
-		$leadId = -1;
-	}
 	require_once 'account.php';
 	$account = new Account($amoHost);
 	require_once 'controller.php';
 	$controller = new Controller($account);
 
-	require_once 'contact.php';
-	$contact = new Contact($entityData, $account->getCustomFields());
+	if ($entityType == 'contacts') {
+		$leadId = $entityData['linked_leads_id'];
+		if (is_array($leadId)) {
+			$leadId = array_key_first($leadId);
+		}
+		else {
+			$leadId = -1;
+		}
+		require_once 'contact.php';
+		$contact = new Contact($entityData, $account->getCustomFields());
 		$resId = $contact->createFromAmo();
 		$check = $controller->checkClient($contact, 'amo');
 		$ycId = ($check['yc_id']>0)?$check['yc_id']:-1;
 		$resultDb = $controller->recordContactFromAmo($contact, $ycId);
 		echo json_encode($resultDb, JSON_UNESCAPED_UNICODE);
-		$controller->recordHook('newtes2t '. json_encode($resultDb, JSON_UNESCAPED_UNICODE));
-
+		$controller->recordHook('amocontact '. json_encode($resultDb, JSON_UNESCAPED_UNICODE));
+	}
+	else {
+		$controller->recordHook('amolead '. json_encode($postData, JSON_UNESCAPED_UNICODE));
+	}
+	
 		/*
 		
 		if ($resultDb) {
