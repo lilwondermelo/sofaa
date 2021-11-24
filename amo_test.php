78466f67a2531f76f0f9665a9d41b80787dfbdc0
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-24 13:19:24 +0700

    Update amo_test.php

--- amo_test.php
+++ amo_test.php
@@ -28,7 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$ycId = ($check['yc_id']>0)?$check['yc_id']:-1;
 		$resultDb = $controller->recordContactFromAmo($contact, $ycId, $leadId);
 		echo json_encode($leadId, JSON_UNESCAPED_UNICODE);
-		$controller->recordHook('amocontact '. json_encode($leadId, JSON_UNESCAPED_UNICODE));
+		$controller->recordHook('amocontact '. json_encode($amoId, JSON_UNESCAPED_UNICODE));
 	}
 	else {
 		sleep(2);
