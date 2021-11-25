9f25238964f2a4e97a111c282004433a81006d56
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-25 16:33:27 +0700

    Update test.php

--- test.php
+++ test.php
@@ -3,5 +3,7 @@
 					$account = new Account('jkamogolovaorg', 'amoContact');
 					require_once 'controller.php';
 					$controller = new Controller($account);
+					
+					echo json_encode($account->getAmoHost(), JSON_UNESCAPED_UNICODE);
 					echo json_encode($controller->testAmo(), JSON_UNESCAPED_UNICODE);
 ?>
