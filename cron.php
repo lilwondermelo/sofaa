da9207721516224dde752237cfb0ea8dcc1ad2f8
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-25 15:32:33 +0700

    Update cron.php

--- cron.php
+++ cron.php
@@ -6,7 +6,7 @@
 			];
 		
 			require_once '_dataSource.class.php';
-			'select cl.amo_host as amoHost, GROUP_CONCAT(r.services) as services, r.filial_id as filial, r.datetime as dateTime, c.lead_id as leadId, GROUP_CONCAT(r.record_id) as recordId from records r 
+			$query24 = 'select cl.amo_host as amoHost, GROUP_CONCAT(r.services) as services, r.filial_id as filial, r.datetime as dateTime, c.lead_id as leadId, GROUP_CONCAT(r.record_id) as recordId from records r 
 			join clients_yc c on r.client_id = c.yc_id
 			join clients cl on c.lead_id = cl.lead_id
 			and r.datetime >= ' . strtotime(date('Y-m-d H:i:s')) . ' 
