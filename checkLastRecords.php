ce34667a2f9e39165a1868eedcaacab162358f1e
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-27 02:22:04 +0700

    Update checkLastRecords.php

--- checkLastRecords.php
+++ checkLastRecords.php
@@ -8,13 +8,10 @@ if (!empty($_GET["company"])) {
 if (!empty($_GET["page"])) {
 	$page = (!empty($_GET["page"]))?$_GET["page"]:'';
 }
-if ($company != '') {
-	require_once 'account.php';
-	$account = new Account($company, 'amoContact');
-	require_once 'controller.php';
-	$controller = new Controller($account);
+
+
 	require_once '_dataSource.class.php';
-	$query = 'select * from clients_yc yc join clients c on yc.lead_id = c.lead_id where c.amo_host = "' . $company . '"';
+	$query = 'select * from clients_yc yc join clients c on yc.lead_id = c.lead_id where c.amo_host = "' . $company . '" order by c.amo_id limit ' . 50*($page - 1) . ',50';
 	$dataSource = new DataSource($query);
 	$data = $dataSource->getData();
 $apiCount = 0;
