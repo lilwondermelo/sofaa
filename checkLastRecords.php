9343023446abbb6e352a05b57ac811056d77a4b9
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-26 19:07:49 +0700

    Update checkLastRecords.php

--- checkLastRecords.php
+++ checkLastRecords.php
@@ -9,26 +9,27 @@ if (!empty($_GET["page"])) {
 	$page = (!empty($_GET["page"]))?$_GET["page"]:'';
 }
 if ($company != '') {
-	
+	require_once 'account.php';
+	$account = new Account($company, 'yc');
+	require_once 'controller.php';
+	$controller = new Controller($account);
 	require_once '_dataSource.class.php';
 	$query = 'select * from clients_yc yc join clients c on yc.lead_id = c.lead_id where c.amo_host = "' . $company . '"';
 	$dataSource = new DataSource($query);
 	$data = $dataSource->getData();
 $apiCount = 0;
 	foreach ($data as $item) {
-		require_once 'account.php';
-	$account = new Account($item['amo_host'], 'amoContact');
-	require_once 'controller.php';
-	$controller = new Controller($account);
+		
 		$apiCount++;
 		$active = $controller->getLastRecord($item['yc_id']);
 		$result[] = $controller->setRecordToAmo($active);
-		//$result[] = $item;
+		$activeresult[] = $active;
 		if ($apiCount == 7) {
 				sleep(1);
 				$apiCount = 0;
 			}
 	}
+	echo json_encode($activeresult, JSON_UNESCAPED_UNICODE);
 	echo json_encode($result, JSON_UNESCAPED_UNICODE);
 
 
