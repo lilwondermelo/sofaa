2c5eb85fc8f12796d48e68075395566e82c8b28a
Al Mikhtadov <lilwondermelo@gmail.com>
2021-11-26 18:40:04 +0700

    Update cron.php

--- cron.php
+++ cron.php
@@ -111,7 +111,7 @@
 			$queryR = 'select cl.amo_host as amoHost, GROUP_CONCAT(r.services) as services, r.filial_id as filial, r.datetime as dateTime, c.lead_id as leadId, GROUP_CONCAT(r.record_id) as recordId from records r 
 			join clients_yc c on r.client_id = c.yc_id
 			join clients cl on c.lead_id = cl.lead_id
-			and r.datetime >= ' . strtotime(date('Y-m-d H:i:s') . '-1 day') . ' 
+			and r.datetime <= ' . strtotime(date('Y-m-d H:i:s') . '-1 day') . ' 
 			and r.req = 0 and attendance != -1 
 			and r.deleted = 0 
 			and c.lead_id is not null 
