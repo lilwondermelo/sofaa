4dedf70c3da21f211a3d9e4e3f6e304428b87fe2
Al Mikhtadov <lilwondermelo@gmail.com>
2021-12-09 09:17:59 +0700

    Update newdashboard.php

--- newdashboard.php
+++ newdashboard.php
@@ -1,17 +1,19 @@
 <?php 
 
 require_once '_dataSource.class.php';
-$query = 'select * from managers';
+$query = 'select m.yc_id as ycId, m.name, sum(r.cost) as sum, count(*) as count from managers m 
+join records r on m.yc_id = r.manager_id 
+group by m.id';
 $dataSource = new DataSource($query);
 if ($data = $dataSource->getData()) {
 	echo '<div class="workArea">
 			<div class="managers">';
 	foreach ($data as $manager) {
 		echo '
-			<div class="managersRow" id="manager' . $manager['yc_id'] . '">
+			<div class="managersRow" id="manager' . $manager['ycId'] . '">
 				<div class="managersRowName">' . $manager['name'] . '</div>
-				<div class="managersRowRecords"></div>
-				<div class="managersRowSumm"></div>
+				<div class="managersRowRecords">' . $manager['count'] . '</div>
+				<div class="managersRowSumm">' . $manager['sum'] . '</div>
 				<div class="managersRowStars"></div>
 				<div class="managersRowCheckbox"></div>
 				<div class="managersRowAddStar"></div>
