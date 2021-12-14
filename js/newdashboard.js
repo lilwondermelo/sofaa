0e1c27119f5ad3e1281348228e0f0f3a309288f4
Al Mikhtadov <lilwondermelo@gmail.com>
2021-12-14 15:24:02 +0700

    Update newdashboard.js

--- js/newdashboard.js
+++ js/newdashboard.js
@@ -2,11 +2,11 @@ $(function(){
 	$("#datepicker").datepicker({
 		onSelect: function(date){
 			$('#datepicker_value').val(date);
-			console.log(date);
+			console.log(Date.parse(date));
 		}
 	});
 	$("#datepicker").datepicker("setDate", $('#datepicker_value').val());
 });
 
 
-console.log(Date.parse($('#datepicker_value').val()));
+console.log($('#datepicker_value').val());
