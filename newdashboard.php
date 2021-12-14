2e68e6ae657dd3f8ee6ad50e06847217c44f3010
Al Mikhtadov <lilwondermelo@gmail.com>
2021-12-14 14:57:10 +0700

    Update newdashboard.php

--- newdashboard.php
+++ newdashboard.php
@@ -22,7 +22,7 @@ and mm.date < '. strtotime("today") . '
 group by m.id';
 $dataSource = new DataSource($query);
 if ($data = $dataSource->getData()) {
-	echo strtotime("yesterday");
+	echo strtotime("today -1 day");
 	echo '<div class="workArea">
 			<div class="calendar">Вчера</div>
 			<div class="filials">Филиалы: все</div>
