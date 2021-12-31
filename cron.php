0eaea7c14d5ed836aab02e30f5f041ee54deda2d
Al Mikhtadov <lilwondermelo@gmail.com>
2021-12-31 15:32:23 +0700

    Update cron.php

--- cron.php
+++ cron.php
@@ -218,7 +218,6 @@
 					else {
 						$dataReq = array(
 					'id' => (int)$item['leadId'],
-					'status_id' => $account->getStatuses()['bot'],
 					'custom_fields_values' => 
 					array(
 						array(
