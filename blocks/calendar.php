c15d546826021f3697d7c098eacda07e946cc113
Al Mikhtadov <lilwondermelo@gmail.com>
2022-01-05 16:18:33 +0700

    123

--- blocks/calendar.php
+++ blocks/calendar.php
@@ -4,8 +4,12 @@ $month = date('m');
 $year = date('Y');
 
 $unixTimestamp = strtotime($year . '-' . $month . '-01');
-//dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+$dayNames = ['Monday' => [1, 'Пн'],'Tuesday' => [2, 'Вт'],'Wednesday' => [3, 'Ср'],'Thursday' => [4, 'Чт'],'Friday' => [5, 'Пт'],'Saturday' => [6, 'Сб'], 'Sunday' => [7, 'Вс']];
 $dayOfWeek = date("l", $unixTimestamp);
-echo $dayOfWeek;
+require_once 'application.class.php';
+$app = new Application();
+$daysInMonth = $app->daysInMonth($month, $year);
+
+echo $daysInMonth;
 
  ?>
