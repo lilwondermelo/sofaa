e189baa0a1372c10156497e63187c0391116be5a
Al Mikhtadov <lilwondermelo@gmail.com>
2022-01-05 16:22:58 +0700

    Update calendar.php

--- blocks/calendar.php
+++ blocks/calendar.php
@@ -1,15 +1,14 @@
 <?php 
-
-$month = date('m');
-$year = date('Y');
-
-$unixTimestamp = strtotime($year . '-' . $month . '-01');
-$dayNames = ['Monday' => [1, 'Пн'],'Tuesday' => [2, 'Вт'],'Wednesday' => [3, 'Ср'],'Thursday' => [4, 'Чт'],'Friday' => [5, 'Пт'],'Saturday' => [6, 'Сб'], 'Sunday' => [7, 'Вс']];
-$dayOfWeek = date("l", $unixTimestamp);
 require_once 'application.class.php';
 $app = new Application();
+$month = date('m');
+$year = date('Y');
+$dayNames = ['Monday' => [1, 'Пн'],'Tuesday' => [2, 'Вт'],'Wednesday' => [3, 'Ср'],'Thursday' => [4, 'Чт'],'Friday' => [5, 'Пт'],'Saturday' => [6, 'Сб'], 'Sunday' => [7, 'Вс']];
 $daysInMonth = $app->daysInMonth($month, $year);
 
-echo $daysInMonth;
-
- ?>
+for ($i = 1; $i <= $daysInMonth; $i++) {
+	$unixTimestamp = strtotime($year . '-' . $month . '-' . $i);
+	$dayOfWeek = date("l", $unixTimestamp);
+	echo $i . ' ';
+}
+?>
