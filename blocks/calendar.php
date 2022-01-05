dbba528b645072a11debb864ca950874b817bfd5
Al Mikhtadov <lilwondermelo@gmail.com>
2022-01-05 16:29:32 +0700

    Update calendar.php

--- blocks/calendar.php
+++ blocks/calendar.php
@@ -5,10 +5,19 @@ $month = date('m');
 $year = date('Y');
 $dayNames = ['Monday' => [1, 'Пн'],'Tuesday' => [2, 'Вт'],'Wednesday' => [3, 'Ср'],'Thursday' => [4, 'Чт'],'Friday' => [5, 'Пт'],'Saturday' => [6, 'Сб'], 'Sunday' => [7, 'Вс']];
 $daysInMonth = $app->daysInMonth($month, $year);
-
+echo '<div class="calendarHeadRow row">
+	<div class="calendarHeadRowItem calendarHeadRowItemName">Менеджеры</div>';
 for ($i = 1; $i <= $daysInMonth; $i++) {
 	$unixTimestamp = strtotime($year . '-' . $month . '-' . $i);
 	$dayOfWeek = date("l", $unixTimestamp);
-	echo $i . ' ' . $dayNames[$dayOfWeek][1] . '; ';
+	echo '
+		<div class="calendarHeadRowItem">
+			<div class="calendarHeadRowItemDay">' . $dayNames[$dayOfWeek][1] . '</div>
+			<div class="calendarHeadRowItemNum">' . $i . '</div>	
+		</div>';
 }
+echo '</div>';
 ?>
+
+
+
