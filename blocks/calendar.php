aa7c0db1c891bb0a52fa6f0250d0cca22b9417b5
Al Mikhtadov <lilwondermelo@gmail.com>
2022-01-05 16:07:48 +0700

    Update calendar.php

--- blocks/calendar.php
+++ blocks/calendar.php
@@ -3,7 +3,7 @@
 $month = date('m');
 $year = date('Y');
 
-$unixTimestamp = strtotime(date('Y-m-d'));
+$unixTimestamp = strtotime($year . '-' . $month . '-01');
 //dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
 $dayOfWeek = date("l", $unixTimestamp);
 echo $dayOfWeek;
