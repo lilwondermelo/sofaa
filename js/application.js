ae05de876680a46735420d0198b3e38c3c8552b4
Al Mikhtadov <lilwondermelo@gmail.com>
2022-01-07 22:59:54 +0700

    Update application.js

--- js/application.js
+++ js/application.js
@@ -8,7 +8,7 @@ function menuClick(item) {
 }
 
 $('body').on('click', '.menuItem', function(){
-     console.log( $(this).attr('data-index'));
+     console.log($('.' + $(this).attr('data-index')));
     if (!$('.' + $(this).attr('data-index')).hasClass('blockActive')) {
 
         menuClick($(this));
