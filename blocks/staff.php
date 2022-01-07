2c18d62945c4c49674ace523b5b96baa0f6e4130
Al Mikhtadov <lilwondermelo@gmail.com>
2022-01-07 19:22:46 +0700

    123

--- blocks/staff.php
+++ blocks/staff.php
@@ -1,5 +1,9 @@
 <?php 
-
-
-
- ?>
+require_once '_dataSource.class.php';
+$query = 'select * from managers';
+$dataSource = new DataSource($query);
+if ($data = $dataSource->getData()) {
+	echo '';
+}
+echo 'Добавить'
+?>
