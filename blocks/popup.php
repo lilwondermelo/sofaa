9a07e7720dffc09cfc896cb72dd22d343e07dbed
Al Mikhtadov <lilwondermelo@gmail.com>
2022-01-08 17:23:26 +0700

    123

--- blocks/popup.php
+++ blocks/popup.php
@@ -2,5 +2,10 @@
 	<div class="popupInner">
 		<h3 class="popupHeader">Выберите</h3>
 		<div class="popupArea"></div>
+		<div class="button popupCancel">Отмена</div>
+		<div class="button popupSave">Сохранить</div>
+		<div class="popupClose img">
+			<img src="img/close.svg" alt="">
+		</div>
 	</div>
 </div>
