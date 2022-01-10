965b3e3a736499c5c0579700a957b1dc2e649d25
Al Mikhtadov <lilwondermelo@gmail.com>
2022-01-10 23:35:57 +0700

    Update calendar.js

--- js/calendar.js
+++ js/calendar.js
@@ -94,7 +94,7 @@ function saveCalendar() {
         type: "POST",
         url: "_ajaxListener.class.php",
         data: {classFile: "application.class", class: "Application", method: "saveCalendar",
-        changed: JSON.stringify(changed)
+        changed: changed
         }}).done(function (result) {
         var data = JSON.parse(result);
         if (data.result === "Ok") {
