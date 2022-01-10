6faead87fc5671f416f66490e9be79b634358287
Al Mikhtadov <lilwondermelo@gmail.com>
2022-01-10 18:46:14 +0700

    Update application.class.php

--- application.class.php
+++ application.class.php
@@ -149,16 +149,19 @@ if (!$data = $dataSource->getData()) {
 			<div class="calendarRow row"> 
 				<div class="calendarRowItem calendarRowItemName"><span>' . $manager[0]['name'] . '</span></div>';
 			for ($i = 1; $i <= $daysInMonth; $i++) {
+				$flag = 0;
 				foreach ($manager as $shift) {
-					$flag = 0;
-					if ($shift['day'] == $id) {
-						$flag = 1;
-						$role = $shift['role'];
-						break;
+					if ($shift['day']) {
+						if ($shift['day'] == $id) {
+							$flag = 1;
+							$role = $shift['role'];
+							break;
+						}
 					}
+					
 				}
 				$html .= '
-					<div class="calendarRowItem ' . (($flag == 1)?'selectedDay':'') . '" data-id="' . (($flag == 1)?$role:0) . '"></div>';
+					<div class="calendarRowItem ' . (($flag == 1)?'selectedDay':'') . '" data-id="' . (($flag == 1)?$role:'0') . '"></div>';
 			}
 			$html .= '</div>';
 		}
