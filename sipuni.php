8a07818c4413616167ca932c6b90c1203570ee3a
Al Mikhtadov <lilwondermelo@gmail.com>
2022-01-11 20:19:41 +0700

    Update sipuni.php

--- sipuni.php
+++ sipuni.php
@@ -39,7 +39,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $output = curl_exec($ch);
 curl_close($ch);
 $output = str_getcsv($output, ";", "\"","\\");
-array_unshift($output);
+//array_unshift($output);
 
 $new = array_slice($output, 1);
 $rows = (count($output)-1)/20-1;
