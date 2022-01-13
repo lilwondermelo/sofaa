42153b76657e3d60a4f7cd2fad22bfc3b28ae185
Al Mikhtadov <lilwondermelo@gmail.com>
2022-01-13 23:20:56 +0700

    Update index.php

--- rust/index.php
+++ rust/index.php
@@ -2,5 +2,5 @@
 //require_once $_SERVER['DOCUMENT_ROOT'] . '/rust/application.class.php';
 require_once 'application.class.php';
 $app = new Application();
-echo $app->apiQuery();
+echo json_encode($app->apiQuery());
 ?>
