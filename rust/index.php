257f5cafc184c32142403d65c0f98f4d63b013cf
Al Mikhtadov <lilwondermelo@gmail.com>
2022-01-13 23:22:49 +0700

    Update index.php

--- rust/index.php
+++ rust/index.php
@@ -3,6 +3,7 @@
 require_once 'application.class.php';
 $app = new Application();
 $data = $app->apiQuery();
+//$data['info_updated']
 
-echo $data['info_updated']
+echo json_encode($app->apiQuery());
 ?>
