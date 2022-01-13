7ac28de8faaa2ecad808f62cd6268bfe0d436ced
Al Mikhtadov <lilwondermelo@gmail.com>
2022-01-13 23:28:32 +0700

    Update index.php

--- rust/index.php
+++ rust/index.php
@@ -3,7 +3,18 @@
 require_once 'application.class.php';
 $app = new Application();
 $data = $app->apiQuery();
-//$data['info_updated']
+$updated = $data['info_updated']
+$host = $data['hostname'];
+$online = $data['players'];
+$maxplayers = $data['maxplayers'];
+$ip = $data['ip'];
+$port = $data['port'];
+$version = $data['version'];
 
-echo json_encode($app->apiQuery());
+
+echo 'Сервер: ' . $host . '<br>';
+echo 'Адрес: ' . $ip . ':' . ($port-1) . '<br>';
+echo 'Игроков: ' . $online . '/' . $maxplayers . '<br>';
+echo 'Обновлено: ' . $updated . '<br>';
+
 ?>
