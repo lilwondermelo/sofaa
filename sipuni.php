487739a267a006bdb0b89607f3059ec0eeb25604
Al Mikhtadov <lilwondermelo@gmail.com>
2022-01-14 20:42:15 +0700

    Update sipuni.php

--- sipuni.php
+++ sipuni.php
@@ -2,8 +2,8 @@
 require_once 'application.class.php';
 $app = new Application();
 $user = '077086';
-$from = '12.01.2022';
-$to = '12.01.2022';
+$from = '13.01.2022';
+$to = '13.01.2022';
 $type = '0';
 $state = '0';
 $tree = '';
