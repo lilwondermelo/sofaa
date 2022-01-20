9a938dd727e629387f3d2332218c391aff9eddda
Al Mikhtadov <lilwondermelo@gmail.com>
2022-01-20 07:16:56 +0700

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -100,13 +100,11 @@ console.log(date2);
         	let max = 0;
         	let maxName = 'Нет';
         	$('.managersRowItemRecords').each(function() {
-        		console.log($(this).html());
-        		if (!$(this).parent().attr('id', 'managerHead')) {
         			if (parseInt($(this).html()) > max) {
         				max = parseInt($(this).html());
         				maxName = $(this).parent().find('.managersRowItem managersRowItemName').html();
         			}
-        		}
+        		
         	})
         	console.log(max);
         	console.log(maxName);
