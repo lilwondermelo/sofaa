309e6fcfe2cba4d3c70bbe30e215c46ca9eae17f
Al Mikhtadov <lilwondermelo@gmail.com>
2022-01-31 20:48:52 +0700

    Update telo.php

--- telo.php
+++ telo.php
@@ -1,10 +1,8 @@
 <?php
 $postData = $_POST;
-$postData1 = json_decode(file_get_contents('php://input'), true);
 require_once 'account.php';
 $account = new Account('autobeauty', 'amoContact');
 require_once 'controller.php';
 $controller = new Controller($account);
 $controller->recordHook('post ' . $postData);
-$controller->recordHook('post ' . $postData1);
 ?>
