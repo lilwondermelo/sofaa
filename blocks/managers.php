411d5afc038a68dfd5305c2fcfa88449527f5b68
Al Mikhtadov <lilwondermelo@gmail.com>
2022-03-23 21:47:27 +0700

    123

--- blocks/managers.php
+++
@@ -1,8 +0,0 @@
-<div class="managersInner">
-<?php 
-require_once 'application.class.php';
-$app = new Application();
-echo $app->getActiveManagers('Telo');  
-?>
-</div>
-<script src="js/managers.js"></script>
--- telo.php
+++
@@ -1,8 +0,0 @@
-<?php
-$postData = $_POST;
-require_once 'account.php';
-$account = new Account('autobeauty', 'amoContact');
-require_once 'controller.php';
-$controller = new Controller($account);
-$controller->recordHook('post ' . json_encode($postData));
-?>
