ae24647427e8a429057f0855b159a3b58572b1d5
Al Mikhtadov <lilwondermelo@gmail.com>
2022-05-08 10:38:37 +0700

    Update map.class.php

--- controller/map.class.php
+++ controller/map.class.php
@@ -53,11 +53,9 @@ class Map {
 		session_start();
 		$userId = $_SESSION["userId"];
 		$source = new DataSource('select (select count(*) from units u1 join clients c1 on u1.player = c1.id where u1.player = ' . $userId . ') as count, u.id, u.player, u.tile, u.turns, ul.name from units u join unit_list ul on u.unit_id = ul.id join clients c on u.player = c.id');
-		if (!$data = $source->getData()) {
-			return $this->startGame();
-		}
-		if ($data[0]['count'] == 0) {
-			return $this->startGame();
+		if ((!$data = $source->getData()) || ($data[0]['count'] == 0)) {
+			$this->startGame();
+			return getUnits();
 		}
 		return $data;
 	}
